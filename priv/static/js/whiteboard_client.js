45730133b7967c601d68dc20ee831ceb79dda88b
Spike Grobstein <me@spike.cx>
2014-02-25 21:57:53 -0500

    added some comments and stuff. cleaned things up

--- priv/static/js/whiteboard_client.js
+++ priv/static/js/whiteboard_client.js
@@ -5,9 +5,8 @@
                         // .subscribe( 'update', this, this.receiveUpdate.bind(this) );
 
     this.currentSequence = 0; // sanity checking
-    this.isReplaying = false; // set to true when processing bulk messages.
-    this.replayQueue = [];    // the queue of packets to replay
 
+    // tracking reconnects
     this.connectionCount = 0;
     this.connectionDelay = 0;
 
@@ -15,6 +14,8 @@
     this.connect( host );
   };
 
+  /*  establish a connection to the server.
+   */
   WhiteboardClient.prototype.connect = function() {
     var ws = new WebSocket( this.host );
 
@@ -30,6 +31,8 @@
     this.ws = ws;
   };
 
+  /*  yay! connected!
+   */
   WhiteboardClient.prototype.handleWebsocketConnected = function() {
     console.log('connected websocket!');
     this.connectionCount += 1;
@@ -42,21 +45,29 @@
     this.sendHello();
   };
 
+  /*  when connection closes, attempt to reconnect
+   *  wait a second, retry... then wait longer. max out at 3 second interval
+   */
   WhiteboardClient.prototype.handleWebsocketClose = function() {
     this.messageBus.broadcast( 'ws_close', {} );
 
+    // increase the connection delay
     this.connectionDelay += 1;
 
+    // but no more than 3 second delay
     if ( this.connectionDelay > 3 ) {
       this.connectionDelay = 3;
 
       this.messageBus.broadcast( 'ws_connection_error', {} );
     }
 
-    // console.log('Lost connection, reconnecting... (' + this.connectionDelay + ')');
+    // attempt to reconnect after a certain amount of sleep
     setTimeout( this.connect.bind(this), this.connectionDelay * 1000 );
   };
 
+  /* Process incomming packets from the server
+   * route them accordingly
+   */
   WhiteboardClient.prototype.handleWebsocketMessage = function( packet ) {
     var event = packet.event,
         payload = packet.payload,
@@ -73,10 +84,7 @@
     // replay the bucket
     // when reach the end, flip the switch back.
 
-    if ( sequence ) {
-      if ( ! this.checkSequence( sequence ) ) { return; }
-      this.currentSequence = sequence;
-    }
+    if ( ! this.checkSequence( sequence ) ) { return; }
 
     switch (event) {
       // an info packet. contains some good info.
@@ -104,22 +112,31 @@
     this.send( 'hello', {} );
   };
 
-  // hello packet contains the user list and current sequence of the board
-  // so pull out the user list and trigger the checkSequence() function.
+  /* hello packet contains the user list and current sequence of the board
+   * so pull out the user list and trigger the checkSequence() function.
+   */
   WhiteboardClient.prototype.processHelloPacket = function( payload ) {
     var name = payload.name,
         sequence = payload.sequence, // the hello packet has the sequence in the payload.
         userList = payload.userList;
 
-    console.log('got hello');
-    console.log({client_user_list:userList});
-
+    // notify about an updated user_list
     this.messageBus.broadcast( "user_list", userList );
 
+    // force-check the sequence
     this.checkSequence( sequence );
   };
 
+  /* validate that an incomming packet fits our sequence
+   * each packet should have an incremented sequence number
+   * if sequence is undefined, ignore it; some packets just don't have sequence numbers
+   * this will also have the side-effect of updating the internal sequence counter
+   */
   WhiteboardClient.prototype.checkSequence = function( sequence ) {
+    // if no sequence is passed, then just return true
+    // this means that the packet didn't contain a sequence and this check is moot
+    if ( typeof sequence === 'undefined' ) { return true; }
+
     if ( sequence > this.currentSequence + 1 ) {
       // we have a problem
       console.log("Received a sequence that's out of range (" + sequence + ")");
@@ -127,16 +144,30 @@
       this.requestUpdateRange( this.currentSequence + 1, sequence );
 
       return false;
+
+    } else if ( sequence <= this.currentSequence ) {
+      // this is very bad. we're ahead of the server somehow?
+      alert('A totally fatal error has occurred. Our sequence is too new new.');
+      return false;
     }
+
+    this.currentSequence = sequence;
 
     return true;
   };
 
+   /*  given a from and to sequence, request the data from the server
+   */
   WhiteboardClient.prototype.requestUpdateRange = function( from, to ) {
     console.log("Requensting from " + from + ' to ' + to );
+
     this.send( 'get_range', { from: from, to: to } );
   };
 
+  /*  send a packet to the server
+   *  the packet should have an event and payload
+   *  it'll be stripped of the pointer, if it exists
+   */
   WhiteboardClient.prototype.send = function( event, payload) {
     delete payload.pointer;
 
@@ -150,18 +181,11 @@
     return this;
   };
 
-  // received a draw event from the local whiteboard
+  /*
+   *  received a draw event from the local whiteboard
+   */
   WhiteboardClient.prototype.receiveDraw = function( messageType, message ) {
-    // console.log({ 'type': messageType, 'got message': message });
-
-    this.sendDraw( message );
-  };
-
-  // send a draw message back up to the server
-  WhiteboardClient.prototype.sendDraw = function( message ) {
     this.send( 'draw', message );
-
-    return this;
   };
 
   window.WhiteboardClient = WhiteboardClient;
