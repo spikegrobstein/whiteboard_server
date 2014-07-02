3aa31eecfbfc297403819741b4568032f0cd132a
Spike Grobstein <me@spike.cx>
2014-07-01 22:28:28 -0400

    WIP getting invites working
    writin' a bunch o' sql

--- lib/whiteboard_server/user.ex
+++ lib/whiteboard_server/user.ex
@@ -90,7 +90,14 @@ defmodule WhiteboardServer.User do
 
   def whiteboards( user_id ) do
     sql = '''
-      select name, key from whiteboards where user_id = $1 and active
+      select
+        whiteboards.name,
+        whiteboard_invites.key
+      from whiteboard_invites
+      join whiteboards on ( whiteboard_invites.board_id = whiteboards.id )
+      where
+        whiteboard_invites.user_id = $1
+        and whiteboards.active
     '''
 
     result = query( sql, [ user_id ] )
@@ -106,33 +113,78 @@ defmodule WhiteboardServer.User do
     end
   end
 
-  def create_whiteboard( user_id, name ) do
+  # create a whiteboard for the given user
+  # then create an invite
+  # return the board ID and the invite key.
+  def create_whiteboard( user_id, whiteboard_name ) do
     sql = '''
-      insert into whiteboards
+      with new_whiteboard as (
+        insert into whiteboards
+        (
+          created_at,
+          user_id,
+          name
+        )
+        values
+        (
+          now(),
+          $1,
+          $2
+        )
+      )
+      insert into whiteboard_invites
       (
-        created_at,
-        user_id,
-        name,
-        key
+        created_at, key, user_id, board_id
       )
       values
       (
-        now(),
-        $1,
-        $2,
-        md5( $3 || $4 || now() )
+       now(),
+       md5( $3 || $4 || now() ),
+       $5,
+       ( select id from new_whiteboard )
       )
-      returning key
     '''
 
-    result = query( sql, [ user_id, name, user_id, name ] )
+    result = query( sql, [ user_id, whiteboard_name, user_id, whiteboard_name, user_id ] )
 
+    IO.puts "Ran query: #{ inspect result }"
     case result do
       { :error, error } ->
         IO.puts "Error creating whiteboard: #{ inspect error }"
         { :error }
-      { :ok, 1, _columns, [ {key} ] } ->
-        :gen_server.call( :board_store, { :create, key } )
+      { :ok, 1, _columns, [ {board_id, key} ] } ->
+        IO.puts "Created new whiteboard: #{ board_id } / #{ key }"
+        :gen_server.call( :board_store, { :create, board_id } )
+        { :ok, board_id, key }
+    end
+
+  end
+
+  # create a whiteboard invite for the given board_id for the given user
+  # return the invite key
+  def create_whiteboard_invite( board_id, user_id ) do
+    sql = '''
+      insert into whiteboard_invites
+      (
+        created_at, key, user_id, board_id
+      )
+      values
+      (
+        now(),
+        md5( $1 || $2 || now() ),
+        $3,
+        $4
+      )
+      returning key
+    '''
+
+    result = query( sql, [ user_id, board_id, user_id, board_id ] )
+
+    case result do
+      { :error, error } ->
+        IO.puts "Error creating invite: #{ inspect error }"
+        { :error }
+      { :ok, 1, _columns, [ { key } ] } ->
         { :ok, key }
     end
 
